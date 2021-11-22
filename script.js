ff8e7d0e62757b3ddedec33f05b5f1f27076c038
Kerry Kreiter <43457152+comm-scripter@users.noreply.github.com>
2021-11-22 16:43:38 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,76 @@
+var front = document.querySelector('.face-front');
+var back = document.querySelector('.face-back');
+var flip = document.querySelector('.book-content');
+var book = document.querySelectorAll('.book');
+var front = document.querySelectorAll('#front');
+var contZindex = 2;
+var customZindex = 1;
+const jan = document.getElementById('jan');
+const feb = document.querySelectorAll('a');
+let mar = document.querySelectorAll('#mar');
+let apr = document.querySelectorAll('#apr');
+let may = document.querySelectorAll('#may');
+let jun = document.querySelectorAll('#jun');
+let jul = document.querySelectorAll('#jul');
+let aug = document.querySelectorAll('#aug');
+let sep = document.querySelectorAll('#sep');
+let oct = document.querySelectorAll('#oct');
+let nov = document.querySelectorAll('#nov');
+let dec = document.querySelectorAll('#dec');
+
+document.getElementById('jan').addEventListener('click', function(event){
+    console.log('jan selected');
+});
+
+for (var i = 0; i < book.length; i++) {
+    book[i].style.zIndex = customZindex;
+    customZindex--;
+
+    book[i].addEventListener('click', function(e){
+
+        var tgt = e.target;
+        var bookThis = this;
+
+        bookThis.style.zIndex = contZindex;
+        contZindex++;
+
+        if (tgt.getAttribute('class') == 'face-front') {
+            bookThis.style.zIndex = contZindex;
+            contZindex +=20;
+            setTimeout(function(){
+                //bookThis.style.transform = 'rotateY(-180deg)';//original code
+                bookThis.classList.add("open");//new for mobile compatability
+            }, 500);
+        }
+        if (tgt.getAttribute("class") == 'face-back') {
+            bookThis.style.zIndex = contZindex;
+            contZindex +=20;
+
+            setTimeout(function(){
+                //bookThis.style.transform = 'rotateY(0deg)';//original code
+                bookThis.classList.remove("open");//new for mobile compatability
+            }, 500);
+        }
+
+        if (tgt.getAttribute('id') == 'front') {
+            flip.classList.remove("trnsf-reset");
+            flip.classList.add("trnsf");
+            
+            console.log(back.classList);
+        }
+        if (tgt.getAttribute('id') == 'trsf') {
+            flip.classList.remove("trnsf");
+            flip.classList.add("trnsf-reset");
+            
+            
+        }
+        if (tgt.getAttribute('id') == 'lastPage') {
+            flip.classList.add("trnsf-back");
+        }
+        if (tgt.getAttribute('id') == 'front-back') {
+            flip.classList.remove("trnsf-back");
+            
+        }
+
+    });
+}
